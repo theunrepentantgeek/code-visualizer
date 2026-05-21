80d2ac19ea0d109580569f1055dac62da8045afa
Bevan Arps <bevan.arps@outlook.com>
2026-05-21 08:07:07 +0000

    Remove os.Exit() as unrequired

--- internal/stages/metrics_test.go
+++ internal/stages/metrics_test.go
@@ -1,7 +1,6 @@
 package stages_test
 
 import (
-	"os"
 	"testing"
 
 	. "github.com/onsi/gomega"
@@ -15,7 +14,7 @@ import (
 
 func TestMain(m *testing.M) {
 	filesystem.Register()
-	os.Exit(m.Run())
+	m.Run()
 }
 
 // ---------------------------------------------------------------------------
